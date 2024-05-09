6c0b7da640c84a31f85d97927dfd53fd22db862e
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-05-09 00:36:30 -0400

    MENU PAGE! and BUTTON WORKS!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import "./style.css";
 import generateLandingPage from "./landingPage.js";
+import generateMenuPage from "./menuPage.js";
 
 const homeBtn = document.querySelector(".home");
 const menuBtn = document.querySelector(".menu");
 const aboutBtn = document.querySelector(".about");
 
+const content = document.querySelector("#content");
+
+function clearContent() {
+  while (content.hasChildNodes()) {
+    content.removeChild(content.lastChild);
+  }
+}
+
 generateLandingPage();
+
+const toMenuPage = document.querySelectorAll(".menu");
+toMenuPage.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("IT WORKS");
+    clearContent();
+    generateMenuPage();
+  });
+});
+
+const toHomePage = document.querySelectorAll(".home");
+toHomePage.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("IT WORKS");
+    clearContent();
+    generateLandingPage();
+  });
+});
---
+++ src/menuPage.js
@@ -0,0 +1,75 @@
+import avocado_Uranmaki from "../assets/images/Avocado_Uramaki.png";
+import salmon_Temaki from "../assets/images/Salmon_Temaki.png";
+import sashimi_slices from "../assets/images/sashimi_slices.png";
+
+function generateMenuPage() {
+  const content = document.querySelector("#content");
+
+  const landingMessage = document.createElement("div");
+  landingMessage.classList.add("message");
+  const h1message = document.createElement("h1");
+  h1message.innerText = '"メニュー"';
+  const h3message = document.createElement("h3");
+  h3message.innerHTML = "TODAY'S <span>SPECIALIZED</span> MENU!";
+
+  landingMessage.appendChild(h1message);
+  landingMessage.appendChild(h3message);
+
+  content.appendChild(landingMessage);
+
+  const item1 = generateItem(
+    avocado_Uranmaki,
+    "Avocado Uramaki Sushi Wrapped with Salmon Plate",
+    "14.45",
+    true
+  );
+  const item2 = generateItem(
+    salmon_Temaki,
+    "Salmon Cucumber Temaki Sushi Plate",
+    "12.95",
+    true
+  );
+
+  const item3 = generateItem(
+    sashimi_slices,
+    "Salmon Sake & Tuna Otoro Sashimi Slice (6 pieces)",
+    "8.95",
+    true
+  );
+
+  content.appendChild(item1);
+  content.appendChild(item2);
+  content.appendChild(item3);
+}
+
+function generateItem(image, title, price, button) {
+  const item = document.createElement("div");
+  item.classList.add("info");
+  const sub_info = document.createElement("div");
+  sub_info.classList.add("sub-info");
+  const picture = new Image();
+  picture.src = image;
+  const text_info = document.createElement("div");
+  text_info.classList.add("text-info");
+  const h2 = document.createElement("h2");
+  h2.innerText = title;
+  const li = document.createElement("li");
+  li.innerText = price;
+
+  text_info.appendChild(h2);
+  text_info.appendChild(li);
+  if (button) {
+    const orderBtn = document.createElement("button");
+    orderBtn.innerText = "Add to order!";
+    orderBtn.classList.add("order", "btn");
+    text_info.appendChild(orderBtn);
+  }
+  sub_info.appendChild(picture);
+  sub_info.appendChild(text_info);
+
+  item.appendChild(sub_info);
+
+  return item;
+}
+
+export default generateMenuPage;
